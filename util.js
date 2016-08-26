f0a0b3d9cc04a9d074e46f1832d5f65117ac2958
张鹏波 <pengbozhang@tencent.com>
2016-08-26 16:22:28 +0800

    add floating number operation

--- util.js
+++ util.js
@@ -66,7 +66,86 @@ define(function (require, exports, module) {
                 secure = arguments[5] || false;
             expires ? exp.setTime(exp.getTime() + expires * 24 * 3600 * 1000) : "";
             document.cookie = name + '=' + encodeURIComponent(value) + ( expires ? ';expires=' + exp.toGMTString() : '') + ( path ? ';path=' + path : '') + ( domain ? ';domain=' + domain : '') + ( secure ? ';secure' : '');
+        },
+        /**
+         * 浮点数相加
+         * @param arg1
+         * @param arg2
+         * @returns {number}
+         */
+        accAdd: function(arg1,arg2){
+            var r1,r2,m;
+            try{
+                r1 = arg1.toString().split(".")[1].length
+            }catch(e){r1=0}
+            try{
+                r2 = arg2.toString().split(".")[1].length
+            }catch(e){r2=0}
+            m = Math.pow(10,Math.max(r1,r2));
+            return (arg1 * m + arg2 * m) / m;
+        },
+        /**
+         * 浮点数减法
+         * @param arg1
+         * @param arg2
+         * @returns {string}
+         */
+        accSub: function(arg1,arg2){
+            var r1,r2,m,n;
+            try{
+                r1=arg1.toString().split(".")[1].length
+            }catch(e){
+                r1=0
+            }
+            try{
+                r2=arg2.toString().split(".")[1].length
+            }catch(e){
+                r2=0
+            }
+            m = Math.pow(10,Math.max(r1,r2));
+            n=(r1>=r2)?r1:r2;
+            return ((arg2*m-arg1*m)/m).toFixed(n);
+        },
+        /**
+         * 浮点数乘法
+         * @param arg1, Number or String
+         * @param arg2, Number or String
+         * @returns {number}
+         */
+        accDiv: function(arg1,arg2){
+            var m=0,
+                s1=arg1.toString(),
+                s2=arg2.toString();
+                try{
+                    m+=s1.split(".")[1].length
+                }catch(e){}
+                try{
+                    m+=s2.split(".")[1].length
+                }catch(e){}
+            return Number(s1.replace(".","")) * Number(s2.replace(".","")) / Math.pow(10,m);
+        },
+        /**
+         * 浮点数除法
+         * @param arg1
+         * @param arg2
+         * @returns {number}
+         */
+        accDiv: function(arg1,arg2){
+            var t1=0,t2=0,r1,r2;
+            try{
+                t1=arg1.toString().split(".")[1].length
+            }catch(e){}
+            try{
+                t2=arg2.toString().split(".")[1].length
+            }catch(e){}
+            with(Math){
+                r1=Number(arg1.toString().replace(".",""));
+                r2=Number(arg2.toString().replace(".",""));
+                return (r1/r2)*pow(10,t2-t1);
+            }
         }
+
+
     };
     module.exports = obj;
 });
